1e3f795dff6fe83932af9f933233bc8c0fe659f3
Hamza Arda Dağlı <hamzaardadagli07@gmail.com>
2025-03-07 14:08:34 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,19 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        Recursive1den10a.Recursiveyaz(10);
-//
-//        int x;
-//        int y;
-//        int carpim=Recursive1den10a.RecursiveUsalma(x=5, y=3);
-//        System.out.print("x üzeri y=" +carpim);
-        int[] a ={1,3,6,8,9};
-        int min=Recursive1den10a.Recursivemin(a,2);
-        System.out.println(min);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-
-
-
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
-
 }
